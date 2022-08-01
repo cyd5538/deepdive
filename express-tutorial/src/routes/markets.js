be06555146a5648f1edacd75990d4f343014ad5b
choiyoungjin <cyd5538@naver.com>
2022-08-01 10:49:51 +0900

    query parameters

--- express-tutorial/src/routes/markets.js
+++ express-tutorial/src/routes/markets.js
@@ -1,21 +1,44 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
 const router = Router();
 
 const supermarkets = [
   {
-    store: 'Whole Foods',
+    id: 1,
+    store: "Whole Foods",
+    miles: 0.6,
   },
   {
-    store: 'Trader Joes',
+    id: 2,
+    store: "Trader Joes",
+    miles: 2.5,
   },
   {
-    store: 'Albertsons',
+    id: 3,
+    store: "Albertsons",
+    miles: 2.8,
+  },
+  {
+    id: 4,
+    store: "Trader Joes",
+    miles: 3.5,
+  },
+  {
+    id: 5,
+    store: "Albertsons",
+    miles: 1.8,
   },
 ];
 
-router.get('', (request, response) => {
-  response.send(supermarkets);
+router.get("", (request, response) => {
+  const { miles } = request.query;
+  const parsedMildes = parseInt(miles);
+  if (!isNaN(parsedMildes)) {
+    const filteredStores = supermarkets.filter((s) => s.miles <= miles);
+    response.send(filteredStores)
+  } else {
+    response.send(supermarkets);
+  }
 });
 
 module.exports = router;
