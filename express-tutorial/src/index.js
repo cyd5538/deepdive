0bf3da2dda73a0c2fd4a53f78a10e92da2d4af50
choiyoungjin <cyd5538@naver.com>
2022-08-01 15:21:02 +0900

    Fake Authentication Example

--- express-tutorial/src/index.js
+++ express-tutorial/src/index.js
@@ -1,30 +1,34 @@
 const express = require('express');
-const cookieParer = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const groceriesRoute = require('./routes/groceries');
 const marketsRoute = require('./routes/markets');
+const authRoute = require('./routes/auth');
+
 
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
-app.use(cookieParer());
 app.use(express.urlencoded());
-app.use(session({
-  secret : "ADFAKLFDKLADFKSDKF",
-  resave : false,
-  saveUninitialized: false,
-}))
 
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'APODAJDSDASMCZXMZADASDASDPASDOASDSAK',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use((req, res, next) => {
   console.log(`${req.method}:${req.url}`);
   next();
 });
-
+ 
 app.use('/api/v1/groceries', groceriesRoute);
 app.use('/api/v1/markets', marketsRoute);
+app.use('/api/v1/auth', authRoute);
 
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
-
