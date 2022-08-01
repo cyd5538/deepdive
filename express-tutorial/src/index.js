d2368bbfe41a8c74d44212e34cb92bcbebef87d4
choiyoungjin <cyd5538@naver.com>
2022-08-01 10:21:03 +0900

    Routers

--- express-tutorial/src/index.js
+++ express-tutorial/src/index.js
@@ -1,46 +1,19 @@
-const { response } = require("express");
-const express = require("express");
+const express = require('express');
+const groceriesRoute = require('./routes/groceries');
+const marketsRoute = require('./routes/markets');
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded());
+
 app.use((req, res, next) => {
-    console.log(`${req.method}:${req.url}`);
-    next();
-})
-
-app.listen(PORT, () => console.log(`이 ${PORT}에서 실행중입니다`));
-
-const groceryList = [
-  {
-    item: "milk",
-    quantity: 2,
-  },
-  {
-    item: "cereal",
-    quantity: 1,
-  },
-  {
-    item: "pop-tarts",
-    quantity: 1,
-  },
-];
-
-app.get("/groceries", (req, res) => {
-    res.send(groceryList);
+  console.log(`${req.method}:${req.url}`);
+  next();
 });
 
-app.get("/groceries/:item", (req, res) => {
-    const { item } = req.params;
-    const groceryItem = groceryList.find((g) => g.item === item)
-    res.send(groceryItem);
-})
+app.use('/api/v1/groceries', groceriesRoute);
+app.use('/api/v1/markets', marketsRoute);
 
-app.post("/groceries", (req, res) => {
-  console.log(req.body);
-  groceryList.push(req.body);
-  res.send(201);
-});
-
+app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
