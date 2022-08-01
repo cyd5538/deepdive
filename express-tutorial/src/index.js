41ad6755ba72cf42ff06aa22fddfe3af4955a442
choiyoungjin <cyd5538@naver.com>
2022-08-01 13:46:16 +0900

    session

--- express-tutorial/src/index.js
+++ express-tutorial/src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const cookieParer = require('cookie-parser');
+const session = require('express-session');
 const groceriesRoute = require('./routes/groceries');
 const marketsRoute = require('./routes/markets');
+
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(cookieParer());
 app.use(express.urlencoded());
+app.use(session({
+  secret : "ADFAKLFDKLADFKSDKF",
+  resave : false,
+  saveUninitialized: false,
+}))
+
 
 app.use((req, res, next) => {
   console.log(`${req.method}:${req.url}`);
@@ -17,3 +27,4 @@ app.use('/api/v1/groceries', groceriesRoute);
 app.use('/api/v1/markets', marketsRoute);
 
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
+
